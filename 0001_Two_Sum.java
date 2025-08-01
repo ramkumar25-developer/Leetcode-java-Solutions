20db76d320e6a9b68750b00f09256cf84a8fb089
Ramkumar R <ramkumardev2125@gmail.com>
2025-08-01 18:10:27 +0530

    Create 0001_Two_Sum.java

---
+++ 0001_Two_Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>(nums.length);
+        for(int i=0;i<nums.length;i++){
+            int find = target-nums[i];
+            if(map.containsKey(find)){
+                return new int[] {map.get(find),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return nums;
+    }
+}
